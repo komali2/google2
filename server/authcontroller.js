d7d323d41d861e29170489834f593553acc0cdf7
komali2 <rogersjcaleb@gmail.com>
2016-08-10 10:42:46 -0700

    bp: created basic session example with no client

--- server/authcontroller.js
+++ server/authcontroller.js
@@ -1,7 +1,16 @@
 var api = {};
 
 api.login = function(req, res){
-    res.status(201).send('unimplemented');
+    var sess = req.session;
+    if(sess.views){
+        sess.views++;
+        res.send('expires in', (sess.cookie.maxAge /1000));
+    }
+    else{
+        sess.views = 1;
+        res.send('welcome! Refresh');
+    }
+    // res.status(201).send('unimplemented');
 }
 
 api.register = function(req, res){
--- server/server.js
+++ server/server.js
@@ -4,10 +4,20 @@ var server = require('http').Server(app);
 var port = process.env.PORT || 8080;
 var path = require('path');
 var bodyParser = require('body-parser');
+var session = require('express-session');
 
 process.setMaxListeners(20);
 
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set(1); //trust first proxy
+
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {secure: false}
+}));
 
 require('./router.js')(app, express);
 
