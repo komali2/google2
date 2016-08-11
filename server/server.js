f8a20e90494f905655d12a1d9d24a634c4bcf9a2
komali2 <rogersjcaleb@gmail.com>
2016-08-10 20:16:25 -0700

    bp: using client pools for example request

--- server/server.js
+++ server/server.js
@@ -7,28 +7,69 @@ var bodyParser = require('body-parser');
 var session = require('express-session');
 var pg = require('pg');
 
-var connectstring = process.env.DATABASE_URL;
-console.log(connectstring);
-var client = new pg.Client(connectstring);
-client.connect((err)=>{
-    if(err) throw err;
+const Pool = require('pg-pool');
+const url = require('url')
+
+const params = url.parse(process.env.DATABASE_URL);
+const auth = params.auth.split(':');
+
+const config = {
+  user: auth[0],
+  password: auth[1],
+  host: params.hostname,
+  port: params.port,
+  database: params.pathname.split('/')[1],
+  ssl: false
+};
+
+var pool = new Pool(config);
+
+pool.connect().then((client)=>{
+    client.query('SELECT * FROM test2').then((res)=>{
+        client.release();
+        console.log("hello from", res.rows[0]);
+    })
+    .catch(e =>{
+        client.release();
+        console.error('query error', e.message, e.stack);
+    })
+})
+
+pool.on('error', function(e, client) {
+  // if a client is idle in the pool
+  // and receives an error - for example when your PostgreSQL server restarts
+  // the pool will catch the error & let you handle it here
+  console.log('error in pool', error);
+});
+
+// pool.query('SELECT * FROM test2').then(function(err, result){
+//     if(err) throw err;
+//     console.log('this is result', err, 'and result', result);
+// }).catch((err)=>{
+//     //console.log('err in promise', err);
+// });
+
+
+
+// client.connect((err)=>{
+//     if(err) throw err;
     
-    client.query('CREATE TABLE IF NOT EXISTS test2(ID INT PRIMARY KEY, NAME TEXT)').then(function(){
-        client.query("INSERT INTO test2 VALUES (1, 'hello')", function(err){
-            if(err) throw err;
-        });
-        client.query('SELECT * FROM test2', function(err, result){
-            if(err) throw err;
-            console.log('this is result', result);
-        })
-    }).catch(function(err){
-        console.log(err);
-    });
+//     client.query('CREATE TABLE IF NOT EXISTS test2(ID INT PRIMARY KEY, NAME TEXT)').then(function(){
+//         client.query("INSERT INTO test2 VALUES (1, 'hello')", function(err){
+//             if(err) throw err;
+//         });
+//         client.query('SELECT * FROM test2', function(err, result){
+//             if(err) throw err;
+//             console.log('this is result', result);
+//         })
+//     }).catch(function(err){
+//         console.log(err);
+//     });
 
     // client.end((err)=>{
     //     if(err) throw err;
     // });
-})
+// })
 
 process.setMaxListeners(20);
 
