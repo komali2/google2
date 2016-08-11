fd67c4fcdd5b91e36633f6ccc01dd4d0d97ea791
komali2 <rogersjcaleb@gmail.com>
2016-08-11 11:44:45 -0700

    wip: added pool to authcontroller

--- server/authcontroller.js
+++ server/authcontroller.js
@@ -1,5 +1,21 @@
 var api = {};
+var pg = require('pg');
+const Pool = require('pg-pool');
+const url = require('url')
 
+const params = url.parse(process.env.DATABASE_URL);
+const auth = params.auth.split(':');
+
+const config = {
+  user: auth[0],
+  password: auth[1],
+  host: params.hostname,
+  port: params.port,
+  database: params.pathname.split('/')[1],
+  ssl: false
+};
+
+var pool = new Pool(config);
 
 api.login = function(req, res){
     var sess = req.session;
