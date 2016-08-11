130f59827505ebf92a04da7329185043060a9fce
komali2 <rogersjcaleb@gmail.com>
2016-08-10 17:03:51 -0700

    bp: connected to DB

--- server/server.js
+++ server/server.js
@@ -5,6 +5,18 @@ var port = process.env.PORT || 8080;
 var path = require('path');
 var bodyParser = require('body-parser');
 var session = require('express-session');
+var pg = require('pg');
+
+var connectstring = process.env.DATABASE_URL;
+var client = new pg.Client(connectstring);
+
+client.connect((err)=>{
+    if(err) throw err;
+
+    client.end((err)=>{
+        if(err) throw err;
+    });
+})
 
 process.setMaxListeners(20);
 
